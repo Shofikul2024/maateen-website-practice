a9f00c1e8fb54e5cac807974e1d5499832e374d2
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-26 19:56:31 +0600

    added  list

--- practice-all-file/list_example.py
+++ practice-all-file/list_example.py
@@ -1,4 +1,46 @@
-a=['onion','potato' , 'onion' , 'ginger', 'cucumber']
-print(a)
-print(type(a))
+# a=['onion','potato' , 'onion' , 'ginger', 'cucumber']
+# print(a)
+# print(type(a))
+#
+
+
 '''লিস্টের আইটেমের ডাটা টাইপ যেকোন কিছুই হতে পারে'''
+
+#
+#
+# b = ['onion', 'potato', 'ginger', 'cucumber', 1, 3.1416 ,9.8]
+# print(b)
+# print(type(b))
+# valueCheck=b[3]
+# print(valueCheck)
+#
+# valueCheck=b[4]
+# valueCheck=b[5]
+#
+# print(valueCheck)
+#
+# print(valueCheck)
+# print(valueCheck)
+# valueCheck=b[1]
+# print(type(valueCheck))
+
+
+# update
+
+
+b = ['onion', 'potato', 'ginger', 'cucumber', 1, 3.1416 ,9.8]
+print(b)
+
+b[1]='shofikul'
+print(b)
+b[0]='apple'
+print(b)
+
+b[5]='banaan'
+print(b)
+b[3]=9.8
+print(b)
+
+
+b[4]={"shofikuil":  "islam"}
+print(b)
